b1de822492768801e2a53343bf29fbdf72a42090
tnmlab <107316287+tnmlab@users.noreply.github.com>
2022-06-12 04:01:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>データ分析学研究室</h1>
+      <h2>東京電機大学 前田高志ニコラス研究室</h2>
     </div>
   );
 }
